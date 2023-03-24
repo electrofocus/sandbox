d4a5bd519b3b3875a6020494c387f68e1475575c
Amir Mullagaliev <amirmullagaliev@gmail.com>
2023-03-24 18:13:44 +0500

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module play
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	path, err := os.MkdirTemp("", "playground*")
+	if err != nil {
+		fmt.Printf("can't create temporary directory (%s)", err)
+		return
+	}
+
+	f1, err := os.Create(filepath.Join(path, "main.go"))
+	if err != nil {
+		fmt.Printf("can't create main.go file (%s)", err)
+		return
+	}
+
+	defer f1.Close()
+	f1.WriteString(`package main
+
+func main() {
+	
+}
+`)
+
+	f2, err := os.Create(filepath.Join(path, "go.mod"))
+	if err != nil {
+		fmt.Printf("can't create go.mod file (%s)", err)
+		return
+	}
+
+	defer f2.Close()
+	f2.WriteString(`module playground
+
+go 1.20
+`)
+
+	if _, err := exec.Command("code", path, "--goto", f1.Name()+":4:2").Output(); err != nil {
+		fmt.Printf("can't get command output (%s)", err)
+		return
+	}
+}
